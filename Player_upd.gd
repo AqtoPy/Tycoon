66d6d8e35d8460a9dcfa83efd67d39ef16383e18
AqtoPy <rekikreki66@gmail.com>
2025-06-21 11:41:50 +0300

    Update Player_upd.gd

--- Player_upd.gd
+++ Player_upd.gd
@@ -1,7 +1,58 @@
-func apply_upgrade_effects():
-    var axe_info = UpgradeSystem.get_upgrade_info(UpgradeSystem.UpgradeID.AXE)
-    var pickaxe_info = UpgradeSystem.get_upgrade_info(UpgradeSystem.UpgradeID.PICKAXE)
+extends CharacterBody3D
+
+# Добавляем экспортируемые переменные для UI
+@export var resource_label: Label
+@export var upgrade_buttons: Array[Button]
+
+# Ресурсы игрока
+var resources = {
+    "wood": 0,
+    "stone": 0,
+    "iron": 0,
+    "gold": 0
+}
+
+# Ссылка на систему улучшений
+var upgrade_system: UpgradeSystem
+
+func _ready():
+    # Получаем систему улучшений автоматически
+    upgrade_system = get_node("/root/UpgradeSystem")
+    update_ui()
+
+func add_resource(resource_type: String, amount: int):
+    if resources.has(resource_type):
+        resources[resource_type] += amount
+        update_ui()
+        # Визуальный эффект
+        show_resource_popup(resource_type, amount)
+
+func update_ui():
+    # Обновляем отображение ресурсов
+    resource_label.text = "Дерево: %d\nКамень: %d\nЖелезо: %d\nЗолото: %d" % [
+        resources.wood,
+        resources.stone,
+        resources.iron,
+        resources.gold
+    ]
     
-    # Применяем бонусы к добыче
-    $HarvestComponent.tree_damage_multiplier = axe_info.value
-    $HarvestComponent.rock_damage_multiplier = pickaxe_info.value
+    # Обновляем кнопки улучшений
+    update_upgrade_buttons()
+
+func update_upgrade_buttons():
+    for i in range(upgrade_buttons.size()):
+        var upgrade_id = i
+        var button = upgrade_buttons[i]
+        var upgrade_info = upgrade_system.get_upgrade_info(upgrade_id)
+        
+        button.text = "%s (Ур. %d)" % [upgrade_info.name, upgrade_info.level]
+        button.disabled = not upgrade_system.can_afford_upgrade(upgrade_id, resources)
+
+func apply_upgrade_effects():
+    # Правильное получение системы улучшений
+    var axe_level = upgrade_system.current_levels[UpgradeSystem.UpgradeID.AXE]
+    var pickaxe_level = upgrade_system.current_levels[UpgradeSystem.UpgradeID.PICKAXE]
+    
+    # Применяем эффекты
+    $HarvestComponent.damage_multipliers["wood"] = 1.0 + (axe_level - 1) * 0.5
+    $HarvestComponent.damage_multipliers["stone"] = 1.0 + (pickaxe_level - 1) * 0.5
